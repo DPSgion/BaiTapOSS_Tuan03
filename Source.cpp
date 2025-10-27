766a01e567f52e4167ab267ffc109a0577f0da0e
Trong Tin <trong2004tin@gmail.com>
2025-10-27 11:26:47 +0700

    Kiem tra so hoan thien

--- Source.cpp
+++ Source.cpp
@@ -12,6 +12,17 @@ int tich(int a, int b){
 	return a * b;
 }
 
+bool kiemTraSoHoanThien(int n){
+	if(n<=1) return false;
+	int tong = 0;
+	for(int i =1 ;i <=n/2;i++){
+		if(n%i==0){
+			tong = tong +i;
+		}
+	}
+	return tong == n;
+}
+
 float thuong(int a, int b);
 int main()
 {
@@ -26,7 +37,12 @@ int main()
 	cout << "Tong la: " << tong(x,y) << endl;
 	cout << "Hieu la: " << hieu(x,y) << endl;
 	cout << "Tich la: " << tich(x,y) << endl;
-
+	cout<<"-------KIỂM TRA SỐ HOÀN THIỆN----------------";
+	cout<< "Nhập số muốn kiểm tra:";
+	int a;
+	cin>>a;
+	if(kiemTraSoHoanThien(a))cout<<a<<"la so hoan thien";
+	else cout<<n<<"khong phai la so hoan thien"<<end;
 	system("pause");
 	return 0;
 }
