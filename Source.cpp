593b1c85874b143323bd29f2045599c674497a16
Trong Tin <trong2004tin@gmail.com>
2025-10-27 10:46:02 +0700

    Nhap x,y và tinh tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b){
+	return a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout<<"Nguyen Trong Tin";
+	int x,y;
+	cout<<"Nhap so thu nhat: ";
+	cin>>x;
+	cout<<"Nhap so thu hai: ";
+	cin>>y;
+	
+	cout<< "Tong la:" << tong(x,y)<<endl;
 	system("pause");
 	return 0;
 }
